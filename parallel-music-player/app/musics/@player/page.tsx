c42749023563266a7219c929e9bfe0b82721c60c
Esther <estherbeyond422@gmail.com>
2026-08-28 11:06:36 +0100

    feat: add album cover image to player component

--- parallel-music-player/app/musics/@player/page.tsx
+++ parallel-music-player/app/musics/@player/page.tsx
@@ -3,7 +3,15 @@ import { Pause, Play, SkipBack, SkipForward } from "lucide-react";
 export default function Player() {
   return (
     <div className="fixed bottom-0 left-[35%] w-[40rem] bg-[#171717] p-4 flex items-center justify-between text-white">
-      <div className="w-16 h-16 bg-gray-700 rounded-md"></div>
+      <div className="w-16 h-16 bg-gray-700 rounded-md">
+        <img
+          src="https://images.unsplash.com/photo-1782178394534-63204f8b83a4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8"
+          alt="imag"
+          height={4}
+          width={4}
+          className="h-15 w-15 bg-gray-700 rounded-md"
+        />
+      </div>
       <div className="flex items-center gap-6">
         <SkipBack
           size={24}
